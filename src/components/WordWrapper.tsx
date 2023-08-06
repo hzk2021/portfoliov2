fc436acc4edbcd93eca941c2dd5be7f54277f9a8
hzk2021 <hezhenkai1999@gmail.com>
2023-08-06 19:28:56 +0800

    Add wrapper component

--- src/components/WordWrapper.tsx
+++ src/components/WordWrapper.tsx
@@ -2,13 +2,14 @@ import { Container, Text } from "@chakra-ui/react"
 import { ReactNode } from "react"
 
 type wrapperProps = {
-  h1Text ?: string,
+  h1Text ?: string | JSX.Element | ReactNode,
   content ?: string | JSX.Element | ReactNode
 }
 
 export default function Wrapper(props : wrapperProps) {
   return (
     <Container 
+      fontFamily="Poppins, var(--chakra-fonts-body)"
       color='white'
       _before={{
         content: '"<section>"',
@@ -31,12 +32,12 @@ export default function Wrapper(props : wrapperProps) {
       }}>
 
 
-      <Text fontSize='5xl' 
-            fontWeight='bold'
+      <Text fontSize={['2rem', '2.5rem']} 
+            fontWeight='extrabold'
+
             _before={{
               content: '"<h1>"',
               display: 'block',
-              marginTop: "2em",
               color: "#515152",
               fontFamily: "La Belle Aurore, cursive",
               fontWeight: "bold",
@@ -45,18 +46,19 @@ export default function Wrapper(props : wrapperProps) {
             _after={{
               content: '"</h1>"',
               display: 'block',
-              marginTop: "2em",
               color: "#515152",
               fontFamily: "La Belle Aurore, cursive",
               fontWeight: "bold",
-              fontSize: "1.4rem"
+              fontSize: "1.4rem",
             }}>
 
             {props.h1Text}
       </Text>
 
-      <Text>
-          {props.content}
+      <Text fontSize={['0.9rem', '1rem']} 
+            fontWeight='medium'
+            fontFamily="Poppins, var(--chakra-fonts-body)">
+            {props.content}
       </Text>
 
     </Container>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,31 @@
+import { Box, Button } from "@chakra-ui/react";
 import WordWrapper from "../components/WordWrapper";
 
+const introMessage = <span>
+  Hi,
+  <br/>
+  I'm Zhenkai,
+  <br/>
+  An Aspiring Developer
+  <br/>
+</span>
+
+const content = <span>
+  I love programming! JavaScript & .NET Developer | CarrySharp
+  <br/>
+  Check out my portfolio!
+
+  <Box mt={5}>
+    <Button w={'100%'}>Website</Button>
+  </Box>
+</span>
+
 function Home() {
+  
   return (
     <WordWrapper 
-      h1Text="Hi, I'm Zhenkai, An Aspring Web Developer"
-      content="I love programming!"/>
+      h1Text={introMessage}
+      content={content}/>
   )
 }
 
